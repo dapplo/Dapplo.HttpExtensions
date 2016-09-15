b52321b7abf35412db58a4855a468f6c8e43b80d
Robin <robin@dapplo.net>
2016-09-15 11:50:07 +0200

    Startin work on the OAuth/OAuth1RequestConfiguration.cs, this class doesn't do anything yet... Added missing file rename from previous commit. [release]

---
+++ Dapplo.HttpExtensions.SharedDesktop/OAuth/OAuth1RequestConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Dapplo.HttpExtensions.SharedDesktop.OAuth
+{
+	/// <summary>
+	/// Specify OAuth 1 request configuration
+	/// </summary>
+	public class OAuth1RequestConfiguration : IHttpRequestConfiguration
+	{
+		/// <summary>
+		/// Name of the configuration, this should be unique and usually is the type of the object
+		/// </summary>
+		public string Name { get; } = nameof(OAuth1RequestConfiguration);
+
+		/// <summary>
+		/// The OAuth parameters for this request
+		/// </summary>
+		public IDictionary<string, object> Parameters { get; set; } = new Dictionary<string, object>();
+	}
+}

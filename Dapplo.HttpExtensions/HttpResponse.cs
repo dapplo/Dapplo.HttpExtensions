59eb1eef62d59382410a4c44670dc469af10f97c
Robin <robin@dapplo.net>
2017-01-20 10:09:18 +0100

    Added HttpResponse without respose / error, to support when calls without content still need to check the status code and more. [release]

--- Dapplo.HttpExtensions/HttpResponse.cs
+++ Dapplo.HttpExtensions/HttpResponse.cs
@@ -31,15 +31,10 @@ namespace Dapplo.HttpExtensions
 {
 	/// <summary>
 	///     This container can be used to get the details of a response.
-	///     It also makes it possible to process the error information, and eventually do something different.
 	///     You can specify your own container, by using the HttpAttribute.
 	/// </summary>
-	/// <typeparam name="TResponse">Type for the normal response</typeparam>
-	/// <typeparam name="TErrorResponse">Type for the error response</typeparam>
 	[HttpResponse]
-	public class HttpResponse<TResponse, TErrorResponse>
-		where TResponse : class
-		where TErrorResponse : class
+	public class HttpResponse
 	{
 		/// <summary>
 		///     The Content-Type of the response
@@ -48,6 +43,29 @@ namespace Dapplo.HttpExtensions
 		[HttpPart(HttpParts.ResponseContentType)]
 		public string ContentType { get; set; }
 
+		/// <summary>
+		///     The reponse headers
+		///     Will be filled due to the annotation
+		/// </summary>
+		[HttpPart(HttpParts.ResponseHeaders)]
+		public HttpResponseHeaders Headers { get; set; }
+
+		/// <summary>
+		///     The response http status code
+		///     Will be filled due to the annotation
+		/// </summary>
+		[HttpPart(HttpParts.ResponseStatuscode)]
+		public HttpStatusCode StatusCode { get; set; }
+	}
+
+	/// <summary>
+	///     This container can be used to get the details of a response without content but with a potential error
+	/// </summary>
+	/// <typeparam name="TErrorResponse">Type for the error response</typeparam>
+	[HttpResponse]
+	public class HttpResponseWithError<TErrorResponse> : HttpResponse
+		where TErrorResponse : class
+	{
 		/// <summary>
 		///     The response if there was an error
 		///     Will be filled due to the annotation
@@ -59,26 +77,25 @@ namespace Dapplo.HttpExtensions
 		///     Was there an error?
 		/// </summary>
 		public bool HasError => ErrorResponse != null;
+	}
 
-		/// <summary>
-		///     The reponse headers
-		///     Will be filled due to the annotation
-		/// </summary>
-		[HttpPart(HttpParts.ResponseHeaders)]
-		public HttpResponseHeaders Headers { get; set; }
-
+	/// <summary>
+	///     This container can be used to get the details of a response.
+	///     It also makes it possible to process the error information, and eventually do something different.
+	///     You can specify your own container, by using the HttpAttribute.
+	/// </summary>
+	/// <typeparam name="TResponse">Type for the normal response</typeparam>
+	/// <typeparam name="TErrorResponse">Type for the error response</typeparam>
+	[HttpResponse]
+	public class HttpResponse<TResponse, TErrorResponse> : HttpResponseWithError<TErrorResponse>
+		where TResponse : class
+		where TErrorResponse : class
+	{
 		/// <summary>
 		///     The response, if there was no error
 		///     Will be filled due to the annotation
 		/// </summary>
 		[HttpPart(HttpParts.ResponseContent)]
 		public TResponse Response { get; set; }
-
-		/// <summary>
-		///     The response http status code
-		///     Will be filled due to the annotation
-		/// </summary>
-		[HttpPart(HttpParts.ResponseStatuscode)]
-		public HttpStatusCode StatusCode { get; set; }
 	}
 }
